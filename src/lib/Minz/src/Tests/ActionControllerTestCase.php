5b20dfedcf7faf8b3dd5eb49d03898f98e746446
Marien Fressinaud <dev@marienfressinaud.fr>
2019-12-09 18:45:41 +0100

    tec: Add an ActionControllerTestCase helper
    Controller tests should inherit from this TestCase class to ease som
    tasks such as including "automatically" the controller file and to test
    the status of a Response.

---
+++ src/lib/Minz/src/Tests/ActionControllerTestCase.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Minz\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ActionControllerTestCase extends TestCase
+{
+    /**
+     * Load the controller code, based on the called class.
+     *
+     * It should be called in `setUpBeforeClass`.
+     *
+     * @throws LogicException if the controller file cannot be included
+     *
+     * @return void
+     */
+    public static function includeController()
+    {
+        $function = new \ReflectionClass(get_called_class());
+        $class = $function->getShortName();
+        $class_without_test = strtolower(substr($class, 0, -4));
+        $included = \Minz\ActionController::loadControllerCode($class_without_test);
+        if (!$included) {
+            throw new \LogicException("{$controller_name} file cannot be loaded.");
+        }
+    }
+
+    /**
+     * Assert that a Response is matching the given conditions.
+     *
+     * @param \Minz\Response $response
+     * @param integer $code The HTTP code that the response must match with
+     * @param string $output The rendered output that the response must match
+     *                       with (optional)
+     * @param string[] $headers The headers that the response must contain (optional)
+     */
+    public function assertResponse($response, $code, $output = null, $headers = null)
+    {
+        $this->assertSame($code, $response->code());
+        if ($output !== null) {
+            $this->assertSame($output, $response->render());
+        }
+        if ($headers !== null) {
+            // I would use assertArraySubset, but it's deprecated in PHPUnit 8
+            // and will be removed in PHPUnit 9.
+            $response_headers = $response->headers();
+            foreach ($headers as $header => $value) {
+                $this->assertArrayHasKey($header, $response_headers);
+                $this->assertSame($value, $response_headers[$header]);
+            }
+        }
+    }
+}
