fb81b1be802a8c60cdbaf338ff0363c763fc7750
Marien Fressinaud <dev@marienfressinaud.fr>
2019-12-12 13:05:31 +0100

    Extract application initialization in a class
    I'll need to reuse most of the application initialization from the CLI
    file. Also it makes sense to initialize the router next to the engine,
    in the src directory.
    
    I'm not sure of what to do with the database initialization, but it's
    easier for me in the Application for now.

--- public/index.php
+++ public/index.php
@@ -11,30 +11,16 @@ if (!$environment) {
 \Minz\Configuration::load($environment, $app_path);
 \Minz\Environment::initialize();
 
-// Initialize the database. If the DB exists, the request will fail since
-// tables already exist. We don't care.
-// I'll design a better system later but for now it's good enough.
-$configuration_path = \Minz\Configuration::$configuration_path;
-$schema = file_get_contents($configuration_path . '/schema.sql');
-$database = \Minz\Database::get();
-$database->exec($schema);
-
-// Get the http information related to the current request
+// Get the http information and create a proper Request
 $http_method = $_SERVER['REQUEST_METHOD'];
 $http_uri = $_SERVER['REQUEST_URI'];
 $http_parameters = array_merge($_GET, $_POST);
 
-// Initialize the needed objects, representing the app and the user request.
 $request = new \Minz\Request($http_method, $http_uri, $http_parameters);
-$router = new \Minz\Router();
 
-$router->addRoute('/', 'home#index', 'get');
-$router->addRoute('/', 'subscriptions#handleRequest', 'post');
-
-// Execute the request against the router and get a response from the executed
-// action.
-$engine = new \Minz\Engine($router);
-$response = $engine->run($request);
+// Initialize the Application and execute the request to get a Response
+$application = new \Webubbub\Application();
+$response = $application->run($request);
 
 // Generate the HTTP headers and output. All the side effects must be contained
 // here.
