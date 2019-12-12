d1a8cef25fc9bdcfd2d1aaa205f7275870fed66e
Marien Fressinaud <dev@marienfressinaud.fr>
2019-12-12 09:10:37 +0100

    tec: Add a simple template system to View
    I wanted to reuse the template of the home page for my error pages, but
    didn't want to duplicate the HTML boilerplate.
    
    Adding a `template` method to set a template for the current view was
    really easy and allow to use it from within an included template,
    allowing quite powerful systems.

---
+++ src/lib/Minz/src/Errors/ViewError.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Minz\Errors;
+
+/**
+ * Exception raised when building a View.
+ *
+ * @author Marien Fressinaud <dev@marienfressinaud.fr>
+ * @license http://www.gnu.org/licenses/agpl-3.0.en.html AGPL
+ */
+class ViewError extends \LogicException
+{
+}
--- src/lib/Minz/src/View.php
+++ src/lib/Minz/src/View.php
@@ -13,6 +13,12 @@ class View
     /** @var string */
     private $filepath;
 
+    /** @var string|null */
+    private $template_name;
+
+    /** @var mixed[] */
+    private $template_variables;
+
     /**
      * @param string $filepath
      */
@@ -38,6 +44,56 @@ class View
         include $this->filepath;
         $output = ob_get_clean();
 
+        if ($this->template_name) {
+            $template_filepath = self::templateFilepath($this->template_name);
+            $view = new View($template_filepath);
+            $this->template_variables['content'] = $output;
+            $output = $view->build($this->template_variables);
+        }
+
         return $output;
+    }
+
+    /**
+     * Allow to set a template to the view.
+     *
+     * It must be called from within the view file directly.
+     *
+     * @param string $template_name The name of the file under src/templates/
+     * @param mixed[] $template_variables A list of variables to pass to the template
+     *
+     * @throws \Minz\Errors\ViewError if the template file doesn't exist
+     * @throws \Minz\Errors\ViewError if the template variables aren't an array
+     */
+    private function template($template_name, $template_variables = [])
+    {
+        $template_filepath = self::templateFilepath($template_name);
+        if (!file_exists($template_filepath)) {
+            throw new Errors\ViewError(
+                "{$template_name} template file does not exist."
+            );
+        }
+
+        if (!is_array($template_variables)) {
+            throw new Errors\ViewError(
+                "Template variables parameter must be an array."
+            );
+        }
+
+        $this->template_name = $template_name;
+        $this->template_variables = $template_variables;
+    }
+
+    /**
+     * Helper to find the path to a template file
+     *
+     * @param string $template_name
+     *
+     * @return string
+     */
+    private static function templateFilepath($template_name)
+    {
+        $app_path = Configuration::$app_path;
+        return "{$app_path}/src/templates/{$template_name}";
     }
 }
