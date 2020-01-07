98f4366cf5ce80ff999a2ccd2aa42796d3b9c6bf
Marien Fressinaud <dev@marienfressinaud.fr>
2020-01-07 09:59:12 +0100

    tec: Remove echo in jobs.sh script
    This script is called every 5 or 10 seconds on the server, and the echo
    strings are logged in at least two files (daemon.log and syslog).
    Consequently, it takes a lot of place for nothing. The Minz logs will
    still be logged.

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env sh
 
-echo "Running subscriptions verifications..."
 php ./webubbub --request /subscriptions/verify
-
-echo "Running subscriptions expirations..."
 php ./webubbub --request /subscriptions/expire
-
-echo "Running contents fetching..."
 php ./webubbub --request /contents/fetch
-
-echo "Running contents delivering..."
 php ./webubbub --request /contents/deliver
-
-echo "All finished!"
