6e44bcc691ba11d8d84b2c7a95480257b354cf4b
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-12-10 15:57:29 +0100

    chore: Update the dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,3 +7,11 @@ assert($app_path !== false);
 include $app_path . '/vendor/autoload.php';
 
 \Webubbub\Configuration::load('test', $app_path);
+
+\Minz\Database::reset();
+$schema = @file_get_contents(\Minz\Configuration::$schema_path);
+
+assert($schema !== false);
+
+$database = \Minz\Database::get();
+$database->exec($schema);
